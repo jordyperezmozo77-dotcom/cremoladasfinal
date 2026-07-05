6bfba58e528559b5c827621aeb3eb10a18eca645
jordy <jordyperezmozo77@gmail.com>
2026-07-04 22:19:03 -0500

    agregue la carpeta java scrip y tambien agregando el modal de boostrap en el index

---
+++ java/modal.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  var miModalElemento = document.getElementById('jpmModalAutomatico');
+  
+
+  var miModal = new bootstrap.Modal(miModalElemento);
+  
+
+  miModal.show();
+});
